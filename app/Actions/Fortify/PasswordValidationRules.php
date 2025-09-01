e3d84fcc7446c5ef28c969c6d40bbf49a425d051
Samuel <Samuel@DESKTOP-RE9Q4UF>
2025-09-01 00:33:28 -0400

    aplicando seguridad de contraseña

--- app/Actions/Fortify/PasswordValidationRules.php
+++ app/Actions/Fortify/PasswordValidationRules.php
@@ -4,18 +4,6 @@ namespace App\Actions\Fortify;
 
 use Illuminate\Validation\Rules\Password;
 
-// Require at least 10 characters...
-(new Password)->length(10)
-
-// Require at least one uppercase character...
-(new Password)->requireUppercase()
-
-// Require at least one numeric character...
-(new Password)->requireNumeric()
-
-// Require at least one special character...
-(new Password)->requireSpecialCharacter()
-
 trait PasswordValidationRules
 {
     /**
@@ -23,8 +11,17 @@ trait PasswordValidationRules
      *
      * @return array<int, \Illuminate\Contracts\Validation\Rule|array<mixed>|string>
      */
+    
     protected function passwordRules(): array
     {
-        return ['required', 'string', Password::default(), 'confirmed'];
+        return [
+        'required',
+        'string',
+        Password::min(10)
+            ->mixedCase()
+            ->numbers()
+            ->symbols(),
+        'confirmed',
+    ];
     }
 }
